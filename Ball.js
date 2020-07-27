0569b7a76e1783cbd43ec2a22aebdb6ad98ee271
Aaditya-coder <66256798+Aaditya-coder@users.noreply.github.com>
2020-07-27 16:01:29 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,39 @@
+class Ball {
+
+
+constructor () {
+var  options = {
+ isStatic:false,
+  restitution:0.3,
+ frictioin:0.5,
+ density:1.2
+
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
